c5dd50ee1bc4dc49cc57d0f5fd0066fe76f027fa
edivaldo soares <edivaldo100@gmail.com>
2020-07-18 16:48:42 -0300

    estrutura inicial do projeto DEMO STORE API

---
+++ src/main/java/com/edivaldo/api/DemoStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.edivaldo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edivaldo/api/DemoStoreApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.edivaldo.api;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
