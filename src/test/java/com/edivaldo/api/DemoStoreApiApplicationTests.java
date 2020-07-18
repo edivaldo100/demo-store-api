20211f12a2523997c535dcbeb05e1abab4d98954
edivaldo soares <edivaldo100@gmail.com>
2020-07-18 17:40:12 -0300

    incluir public class test

--- src/test/java/com/edivaldo/api/DemoStoreApiApplicationTests.java
+++ src/test/java/com/edivaldo/api/DemoStoreApiApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoStoreApiApplicationTests {
+public class DemoStoreApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
