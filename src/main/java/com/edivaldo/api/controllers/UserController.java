860ddabcc7345e8c86372a91bb79597065674b4b
edivaldo soares <edivaldo100@gmail.com>
2020-07-19 23:34:52 -0300

    implementação de update de usuario

--- src/main/java/com/edivaldo/api/controllers/UserController.java
+++ src/main/java/com/edivaldo/api/controllers/UserController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.edivaldo.api.dtos.UserDto;
 import com.edivaldo.api.response.Response;
 import com.edivaldo.api.services.UserService;
-import com.kazale.pontointeligente.api.dtos.FuncionarioDto;
 
 @RestController
 @RequestMapping("/api/users")
