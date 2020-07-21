fc668a26cac06db3e8e7c57495fbe3a3d20101a9
edivaldo soares <edivaldo100@gmail.com>
2020-07-21 20:51:39 -0300

    ajuste heroku 6

--- src/main/resources/db/migration/mysql/V4__ajusteTBPedido.sql
+++ src/main/resources/db/migration/mysql/V4__ajusteTBPedido.sql
@@ -15,8 +15,8 @@ insert into product (id, name, price)
 	values (null, 'Foguete modelo 2000', 700000);
 	
 	
-insert into store.ordered (id, user_id) 
+insert into ordered (id, user_id) 
 	values (null,1);
 	
- insert into store.product_item (id, ordered_id, product_id) 
+ insert into product_item (id, ordered_id, product_id) 
 	values (null, 1,1);
